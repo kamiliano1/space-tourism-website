dc1d070244f54f6b950ad0ed15de4fa65e7aec3f
kamiliano1 <kamiliano1991@gmail.com>
2023-01-20 22:18:50 +0100

    Navbar
    Added media query for mobile, tablet and desktop according to the desing
    Mobile nav button was created using useState and when is true a new class is added to show the navbar

--- space-tourism/src/components/Navbar.jsx
+++ space-tourism/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import spaceLogo from "/assets/shared/logo.svg"
+import openNavButton from "/assets/shared/icon-hamburger.svg"
+import closeNavButton from "/assets/shared/icon-close.svg"
 
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    
     return (
-        <header className="primary-header flex">
-            <img src={spaceLogo} alt="space logo"/>
+        <header className="primary-header container flex">
+            <img src={spaceLogo} alt="space logo" className="logo"/>
             <nav>
-                <ul className="flex primary-navigation indicators underline-indicators">
+                <button onClick={()=>setIsOpen(prev=>!prev)} className="nav-button">
+                    <img src={isOpen ? closeNavButton : openNavButton} alt="Menu button" />
+                    <span className="sr-only">Menu</span>
+                </button>
+                <ul className={`flex primary-navigation indicators underline-indicators ${isOpen ? "opened-navigation" : ""}`}>
                     <li className="active">
                         <Link to="/"  className="upper-case fs-400 ff-barlow letter-spacing-270">
                             <span aria-hidden="true">00</span>Home
@@ -30,26 +41,28 @@ export default function Navbar() {
 
                 </ul>
 
-                <Link to="/destination" className="explore-button upper-case fs-600 ff-belle f-c-dark bg-c-white letter-spacing-270">
+            </nav>
+                {/* <Link to="/destination" className="explore-button upper-case fs-600 ff-belle f-c-dark bg-c-white letter-spacing-270">
                             Explore
                 </Link>
+
                 <div className="indicators underline-indicators tabs-indicators flex">
                     <button aria-selected="true" aria-controls="moon-tab" className="upper-case f-c-light ff-barlow ">Moon</button>
                     <button aria-selected="false" aria-controls="mars-tab" className="upper-case f-c-light ff-barlow ">Mars</button>
                     <button aria-selected="false" aria-controls="europa-tab" className="upper-case f-c-light ff-barlow"  >Europa</button>
                 </div>
+                
                 <div className="indicators dots-indicators flex">
                     <button aria-selected="true" aria-controls="moon-tab" className="upper-case f-c-light ff-barlow "><span class="sr-only">Moon</span></button>
                     <button aria-selected="false" aria-controls="mars-tab" className="upper-case f-c-light ff-barlow "><span class="sr-only">Mars</span></button>
                     <button aria-selected="false" aria-controls="europa-tab" className="upper-case f-c-light ff-barlow"  ><span class="sr-only">Europa</span></button>
                 </div>
 
-            </nav>
                 <div className="indicators buttons-indicators grid">
                     <button aria-selected="true" aria-controls="moon-tab" className="fs-600 bg-c-dark f-c-white ff-belle"><span class="sr-only">Moon</span>1</button>
                     <button aria-selected="false" aria-controls="mars-tab" className="fs-600 bg-c-dark f-c-white ff-belle"><span class="sr-only">Mars</span>2</button>
                     <button aria-selected="false" aria-controls="europa-tab" className="fs-600 bg-c-dark f-c-white ff-belle"  ><span class="sr-only">Europa</span>3</button>
-                </div>
+                </div> */}
  
 
         </header>
--- space-tourism/src/pages/Home.jsx
+++ space-tourism/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar"
 export default function Home() {
     return (
-        <div className="fs-900">
+        <div className="fs-200">
             HOME
         </div>
     )
